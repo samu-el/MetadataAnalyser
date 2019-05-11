76a0f37093469751923aafb76b88519cbc591b43
Samuel Mussie <punisami@gmail.com>
2019-05-11 12:24:22 +0300

    basic working prototype

--- analyser.py
+++ analyser.py
@@ -7,13 +7,17 @@ import sys
 from PyPDF2 import PdfFileReader
 import exifread
 import docx
-# from file_metadata.generic_file import GenericFile
+import pprint
+
+#https://github.com/atdsaa/file-metadata with few modifications to  utilities.py
+from file_metadata.generic_file import GenericFile
 
 
 
-# def get_generic_file_info(path):
-# 	gf = GenericFile.create(path)
-# 	gf.analyse()
+
+def get_generic_file_info(path):
+	gf = GenericFile.create(path)
+	return gf.analyze()
 
 
 
@@ -21,46 +25,46 @@ import docx
 def get_docx_info(path):
 	document = docx.Document(docx = path)
 	core_properties = document.core_properties
-	print("Created:", core_properties.created)
-	print("Last Modified By:", core_properties.last_modified_by)
-	print("Last Printed:", core_properties.last_printed)
-	print("Modified:", core_properties.modified)
-	print("Revision:", core_properties.revision)
-	print("Title:", core_properties.title)
-	print("Category:", core_properties.category)
-	print("Comments:", core_properties.comments)
-	print("Identifier:", core_properties.identifier)
-	print("Keywords:", core_properties.keywords)
-	print("Language:", core_properties.language)
-	print("Subject:", core_properties.subject)
-	print("Version:", core_properties.version)
-	print("Keywords:", core_properties.keywords)
-	print("Content Status:", core_properties.content_status)
+	info = { 
+		"created":core_properties.created,
+		"last_modified_by": core_properties.last_modified_by,
+		"last_printed": core_properties.last_printed,
+		"modified": core_properties.modified,
+		"revision": core_properties.revision,
+		"title": core_properties.title,
+		"category": core_properties.category,
+		"comments": core_properties.comments,
+		"identifier": core_properties.identifier,
+		"keywords": core_properties.keywords,
+		"language": core_properties.language,
+		"subject": core_properties.subject,
+		"version": core_properties.version,
+		"keywords": core_properties.keywords,
+		"content_status": core_properties.content_status
+	}
+	return info
 
 def get_image_info(path):
 	with open(path, 'rb') as f:
-		tags = exifread.process_file(f)
+		tags = exifread.process_file(f)	
 		for tag in tags.keys():
 			if tag not in ('JPEGThumbnail', 'TIFFThumbnail', 'Filename', 'EXIF MakerNote'):
 				print("%s: %s" % (tag, tags[tag]))
+	return tags
 
  
 def get_pdf_info(path):
     with open(path, 'rb') as f:
         pdf = PdfFileReader(f)
         info = pdf.getDocumentInfo()
-        number_of_pages = pdf.getNumPages()
+        info['number_of_pages'] = pdf.getNumPages()
+	   	
+    return info
 
-    author = info.author
-    creator = info.creator
-    producer = info.producer
-    subject = info.subject
-    title = info.title
-   
-    print(info)
- 
 if __name__ == '__main__':
     path = sys.argv[1]
     # get_pdf_info(path)
     # get_image_info(path)
-    get_docx_info(path)
+    # get_docx_info(path)
+    info = get_generic_file_info(path)
+    pprint.pprint(info)
--- bot.py
+++ bot.py
@@ -1,25 +1,28 @@
 #! python3
 import sys
 import time
+import datetime
+import json
+
 import telepot
 from telepot.loop import MessageLoop
 from telepot.namedtuple import InlineQueryResultArticle, InputTextMessageContent
-import datetime
+
+
 
 from maclookup import ApiClient
-import logging
 
 import pymongo
 from pymongo import MongoClient
+
+import analyzer
 
 mac_client = ApiClient('APIKEY')
 
 bot = telepot.Bot('APIKEY')
 
-answerer = telepot.helper.Answerer(bot)
-
 connection_params = {
-    'user': '<USERNAME>',
+    'user': '<USERNAME',
     'password': '<PASSWORD>',
     'host': 'example.mlab.com',
     'port': 54321,
@@ -39,25 +42,42 @@ botResponse = db.response
 
 def handle(msg):
     try:
+        content_type, chat_type, chat_id = telepot.glance(msg)
+        print(content_type,chat_type, chat_id)
         person_id = msg.get('chat').get('id')
         text = msg.get('text')
         bot.sendChatAction(person_id, "typing")
         log(msg)
         userMsg.insert_one(msg)
-        if text.startswith('/mac'):
+        if text and text.startswith('/mac'):
             address = text.strip().split(' ')[-1].strip()
             info = mac_client.get_raw_data(address, 'json')
             bot.sendMessage(
-                person_id, info)
+                person_id, "`"+str(info)+"`", parse_mode='markdown')
+        elif content_type == 'document':
+            file_id = msg['document']['file_id']
+            file_name =  msg['document']['file_name']
+            bot.download_file(file_id, file_name)
+            info = analyzer.get_generic_file_info('./'+file_name)
+            bot.sendMessage(
+                person_id, "`"+str(info)+"`", parse_mode='markdown')
+        elif content_type == 'photo':
+            file_id = msg['photo'][-1]['file_id']
+            file_name =  msg['photo'][-1]['file_id']
+            bot.download_file(file_id, file_name)
+            info = analyzer.get_generic_file_info('./'+file_name)
+            bot.sendMessage(
+                person_id, "`"+str(info)+"`", parse_mode='markdown')
         else:            
-            result = "Please Choose one of the commands"
-            bot.sendMessage(person_id, result)
-            botResponse.insert_one({"person_id:": person_id, "result": result});
+            info = "Please Choose one of the commands"
+            bot.sendMessage(person_id, info)
+            botResponse.insert_one({"person_id:": person_id, "info": str(info)});
     except Exception as e:
         bot.sendMessage(
             person_id, "Something went wrong, please try again later")
         errors.insert_one({'error':str(e), "line": 84})
         log("Handle: Error: "+str(e))
+        raise e
 
 
 def get_results(response):
@@ -65,7 +85,6 @@ def get_results(response):
 
 def format_results(result):
     pass
-
 
 def log(msg):
     with open('log_results.txt', 'a') as logfile:
