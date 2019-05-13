63ea14c60ca16326f92d585a4ad8128218f96b77
AAITbereket <bereketgebredingle@gmail.com>
2019-05-13 18:01:59 +0300

    bot response beutified

--- bot.py
+++ bot.py
@@ -15,7 +15,7 @@ from maclookup import ApiClient
 import pymongo
 from pymongo import MongoClient
 
-import analyzer
+import analyser
 
 mac_client = ApiClient('APIKEY')
 
@@ -40,10 +40,11 @@ userMsg = db.messages
 errors = db.errors
 botResponse = db.response
 
+
 def handle(msg):
     try:
         content_type, chat_type, chat_id = telepot.glance(msg)
-        print(content_type,chat_type, chat_id)
+        print(content_type, chat_type, chat_id)
         person_id = msg.get('chat').get('id')
         text = msg.get('text')
         bot.sendChatAction(person_id, "typing")
@@ -52,30 +53,35 @@ def handle(msg):
         if text and text.startswith('/mac'):
             address = text.strip().split(' ')[-1].strip()
             info = mac_client.get_raw_data(address, 'json')
+            print (type(info))
+            info = format_results(info)
             bot.sendMessage(
-                person_id, "`"+str(info)+"`", parse_mode='markdown')
+                person_id, "`"+info+"`", parse_mode='markdown')
         elif content_type == 'document':
             file_id = msg['document']['file_id']
-            file_name =  msg['document']['file_name']
+            file_name = msg['document']['file_name']
             bot.download_file(file_id, file_name)
-            info = analyzer.get_generic_file_info('./'+file_name)
+            info = analyser.get_generic_file_info('./'+file_name)
+            info = format_results(info)
             bot.sendMessage(
                 person_id, "`"+str(info)+"`", parse_mode='markdown')
         elif content_type == 'photo':
             file_id = msg['photo'][-1]['file_id']
-            file_name =  msg['photo'][-1]['file_id']
+            file_name = msg['photo'][-1]['file_id']
             bot.download_file(file_id, file_name)
-            info = analyzer.get_generic_file_info('./'+file_name)
+            info = analyser.get_generic_file_info('./'+file_name)
+            info = format_results(info)
             bot.sendMessage(
-                person_id, "`"+str(info)+"`", parse_mode='markdown')
+                person_id, "`"+info+"`", parse_mode='markdown')
         else:            
             info = "Please Choose one of the commands"
             bot.sendMessage(person_id, info)
-            botResponse.insert_one({"person_id:": person_id, "info": str(info)});
+            botResponse.insert_one(
+                {"person_id:": person_id, "info": str(info)})
     except Exception as e:
         bot.sendMessage(
             person_id, "Something went wrong, please try again later")
-        errors.insert_one({'error':str(e), "line": 84})
+        errors.insert_one({'error': str(e), "line": 84})
         log("Handle: Error: "+str(e))
         raise e
 
@@ -84,7 +90,17 @@ def get_results(response):
     pass
 
 def format_results(result):
-    pass
+    # Casting to JSON 
+    if(type(result) == dict):
+        str_json = json.dumps(result)
+        decoded_json = json.loads(str_json)
+    elif (type(result) == bytes ):
+        decoded_json = json.loads(result.decode('utf8').replace("'", '"'))
+
+    # Pretty Print the JSON
+    ret = json.dumps(decoded_json, indent=2, sort_keys=True)
+    return ret
+
 
 def log(msg):
     with open('log_results.txt', 'a') as logfile:
@@ -96,11 +112,13 @@ def main():
     MessageLoop(bot, {'chat': handle}).run_as_thread()
 
     print('Listening ...')
-    log("******************************Server Started at"+datetime.datetime.now().isoformat()+"***************************")
+    log("******************************Server Started at" +
+        datetime.datetime.now().isoformat()+"***************************")
 
     # Keep the program running.
     while 1:
         time.sleep(10)
+
 
 if __name__ == '__main__':
     main()
